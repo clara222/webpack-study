e7a1db281c17bf55fad3977b2c19997741fa1e69
nana <wangruina@xin.com>
2019-04-18 15:05:00 +0800

    在业务代码中使用babel编译es6新语法

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import "@babel/polyfill";
 import pic from './images/timg.jpg'
 import './scss/index.scss'
 var img = new Image()
@@ -6,3 +7,11 @@ img.classList.add('avatar')
 document.body.append(img)
 var root = document.getElementById('root')
 root.innerHTML='<div class="iconfont icon-icon_collect color"></div>'
+
+const foo = () => {
+  return new Promise((resolve, reject) => {
+    resolve(99)
+  })
+}
+
+console.log(foo());
--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,16 @@ module.exports = {
   },
   module: {
     rules: [
+      { 
+        test: /\.js$/, 
+        exclude: /node_modules/, 
+        loader: "babel-loader",
+        options: {
+          "presets": [["@babel/preset-env",{
+            useBuiltIns: 'usage'
+          }]]  
+        }
+      },
       {
         test: /\.(jpg|png|jpeg|gif)$/,
         use: {
